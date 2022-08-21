16ea7e03411ad5168032128210b8f653c65a56ec
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2022-08-21 14:43:09 +0300

    doing stuff

---
+++ hooks/useBreakpoints.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+export default function useDimensions() {
+  const [dimensions, setDimensions] = useState({
+    w: undefined,
+    h: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      console.log('WINDOW IS UNDEFINED');
+      return;
+    }
+
+    const detectSize = () => {
+      setDimensions({
+        w: window.innerWidth,
+        h: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', detectSize);
+
+    return () => {
+      console.log('REMOVE EVENT LISTENER');
+      window.removeEventListener('resize', detectSize);
+    };
+  }, []);
+
+  // return {
+  //   width: dimensions.w,
+  //   height: dimensions.h,
+  // };
+
+  return {
+    sm: dimensions.w <= 640,
+    md: dimensions.w >= 641 && dimensions.w <= 768,
+    lg: dimensions.w >= 769 && dimensions.w <= 1024,
+    // xl: dimensions.w >= 1025,
+    xl: dimensions.w >= 1025 && dimensions.w <= 1280,
+    '2xl': dimensions.w >= 1281,
+  };
+}
--- pages/artists/[slug].js
+++ pages/artists/[slug].js
@@ -1,26 +1,45 @@
 import Image from 'next/image';
+import { userAgent } from 'next/server';
+import { isMobile } from 'react-device-detect';
 import HeadingHero from '../../components/hero/headingHero';
 import Layout from '../../components/layout';
+import useDimensions from '../../hooks/useBreakpoints';
 import { getAllArtistSlugs, getArtistWithSlug } from '../../lib/contentful';
+
+const shouldShowPortait = (breakpoints) => {
+  // The initial render is done server side -> window is undefined -> all breakpoints are `false`
+  // In this case, show portrait based on device
+  if (Object.values(breakpoints).filter((x) => x).length === 0) {
+    return !isMobile;
+  }
+
+  return !!breakpoints['2xl'];
+};
 
 export default function ArtistPage({
   artist: { title, description, imageLandscape, imagePortrait },
 }) {
+  const breakpoints = useDimensions();
+
   return (
     <Layout title={title} ogImage={imageLandscape.url}>
       <div className="full-height flex flex-col xl:flex-row">
         <div className="flex-1 relative">
           <Image
             alt={title}
-            src={imagePortrait.url}
+            src={
+              shouldShowPortait(breakpoints)
+                ? imagePortrait.url
+                : imageLandscape.url
+            }
             layout="fill"
             objectFit="cover"
             priority={true}
           />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 px-8">
           <HeadingHero text={title} />
-          <p className="p-8">{description}</p>
+          <p className="">{description}</p>
         </div>
       </div>
     </Layout>
